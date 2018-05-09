73f52d322aa3fd4acc8af78f4db1a63c80d6395c
Ryan Braving <ryan.braving@gmail.com>
2018-05-10 00:08:30 +1200

    20180510-PlyrSingleDone

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 // import $ from 'jquery';
 // import 'mediaelement/full';
 import './main.html';
+import Plyr from 'plyr';
+
 
 
 
@@ -37,6 +39,16 @@ Template.player.helpers({
   sounds: function(){
     return Sounds.find({});
   },
+
+  tasksLoaded: function () {
+    return Session.get('tasksLoaded');
+  },
+});
+
+// Meteor.subscribe("sounds");
+
+Meteor.subscribe('sounds', function onReady() {
+  Session.set('tasksLoaded', true);
 });
 
 // Template.player.onRendered(function() {
@@ -44,16 +56,59 @@ Template.player.helpers({
 
 // });
 
-// Template.player.onRendered(function() {
-//       $(document).ready(function() {
-//         var script = document.createElement("script");
-//         script.type="text/javascript";
-//         script.src = "https://cdn.plyr.io/3.3.6/plyr.js";
-//         $("#script_div").append(script);
-//       });
-// });
+Template.player.onRendered(function() {
+      $(document).ready(function() {
+        var script = document.createElement("script");
+        script.type="text/javascript";
+        script.src = "https://cdn.plyr.io/3.3.7/plyr.js";
+        $("#script_div").append(script);
+      });
+      // const player = new Plyr('#player');
+});
 
 Template.player.onRendered(function() {
-   $.getScript("https://cdn.plyr.io/3.3.6/plyr.js");
+   $.getScript("https://cdn.plyr.io/3.3.7/plyr.js");
    const player = new Plyr('#player');
+});
+
+// Template.player.onRendered(function() {
+//    $.getScript("https://cdn.plyr.io/3.3.6/plyr.js");
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+  // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+  const player = new Plyr('.player');
+  // const player = new Plyr(document.getElementById('player'));
+
+  // Bind event listener
+  function on(selector, type, callback) {
+    document.querySelector(selector).addEventListener(type, callback, false);
+  }
+
+  // Play
+  on('.js-play', 'click', () => { 
+    player.play();
+  });
+
+  // Pause
+  on('.js-pause', 'click', () => { 
+    player.pause();
+  });
+
+  // Stop
+  on('.js-stop', 'click', () => { 
+    player.stop();
+  });
+
+  // Rewind
+  on('.js-rewind', 'click', () => { 
+    player.rewind();
+  });
+
+  // Forward
+  on('.js-forward', 'click', () => { 
+    player.forward();
+  });
 });
